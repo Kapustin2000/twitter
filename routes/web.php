e24f970521baaf1258220f912c09544a91c23871
Mikhail <kapustinomm@gmail.com>
2020-05-27 00:32:35 +0300

    add vue and register controllers

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', 'TweetsController@index')->name('home');
+});
+
