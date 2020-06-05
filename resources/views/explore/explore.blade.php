fcad07bbd6426991d7a4f0a490fb099854f234a9
Mikhail <kapustinomm@gmail.com>
2020-06-05 19:49:38 +0300

    Fix explore page

--- resources/views/explore/explore.blade.php
+++ resources/views/explore/explore.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <explore-wall :users="{{$users}}"></explore-wall>
+    <ul>
+        @forelse($users as $user)
+        <li class="mt-2">
+            <div>
+                <a href="{{$user->path()}}" class="flex items-center text-sm">
+                    <img src="{{$user->getAvatar()}}" alt="" class="rounded-full mr-2" width="40" height="40">
+                    {{$user->username}}
+                </a>
+            </div>
+        </li>
+        @empty
+        <li v-else>No users yet!</li>
+        @endforelse
+    </ul>
 @endsection
