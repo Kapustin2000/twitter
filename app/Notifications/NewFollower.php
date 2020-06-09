5948bf3460c1dc04edbb58ba0e042bb1836b486d
Mikhail <kapustinomm@gmail.com>
2020-06-09 22:23:19 +0300

    ShouldQueue NewFollower notification

--- app/Notifications/NewFollower.php
+++ app/Notifications/NewFollower.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class NewFollower extends Notification 
+class NewFollower extends Notification implements ShouldQueue
 {
     use Queueable;
 
